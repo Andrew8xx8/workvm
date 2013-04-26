2639f92c48c9a0eaa211c06f74292f48b7c88b7a
Andrew8xx8 <avk@8xx8.ru>
2013-04-26 19:26:50 +0400

    Stuff removed

--- site-cookbooks/rails/metadata.rb
+++
@@ -1,8 +0,0 @@
-maintainer       "8xx8"
-maintainer_email "avk@8xx8.ru"
-license          "All rights reserved"
-description      "Installs/Configures ulway"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
-
-depends "rvm"
