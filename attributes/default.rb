2cf1df6b00ab25708dcaaf8d262e2f5c28f934cb
Andreww8xx8 <avk@8xx8.ru>
2013-11-11 15:59:46 +0400

    Instasll dotfiles

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,3 @@
 default['workvm']['packages'] = %w{
-  vim git-core curl libcurl3-dev sphinxsearch imagemagick
+  exuberant-ctags vim git-core curl libcurl3-dev sphinxsearch imagemagick
 }
-
-default[:ohmyzsh][:theme] = 'blinks'
