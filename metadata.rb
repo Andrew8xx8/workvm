fbb219484c49635c45d01e07f1c89320f273f01d
Andreww8xx8 <avk@8xx8.ru>
2013-11-11 16:03:43 +0400

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Andrew Kulakov"
 maintainer_email "avk@8xx8.ru"
 license          "MIT License"
 description      "General work machine"
-version          "0.1"
+version          "0.2"
 
 %w{ apt zsh }.each do |cb_depend|
   depends cb_depend
